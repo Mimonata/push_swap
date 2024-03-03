8912871f8dbd719317847e3e26148753cd26d0b8
sabina v. cordero <sabina_pitul@yahoo.es>
2024-03-03 21:03:53 +0100

    ops finished

--- operations.c
+++ operations.c
@@ -15,19 +15,16 @@
 void	swap_top(t_node **stack)
 {
 	t_node	*temp;
-	t_node	*last;
-	
+
+	temp = (*stack)->next;	
 	if (!(*stack) || ((*stack)->next == NULL))
 		return ;
-	last = *stack;
-	while (last->next != NULL)
-		last = last->next;
-	temp = last->prev;
-	last->prev = temp->prev;
-	temp->prev->next = last;
-	last->next = temp;
-	temp->prev = last;
-	temp->next = NULL;
+	(*stack)->next = temp->next;
+	temp->next->prev = *stack;
+	(*stack)->prev = temp;
+	temp->prev = NULL;
+	temp->next = *stack;
+	*stack  = temp;
 }
 
 void	sa(t_node **stack_a)
@@ -49,7 +46,7 @@ void	ss(t_node **stack_a, t_node **stack_b)
 	write(1, "ss\n", 3);
 }
 
-t_node	*got_to_last(t_node *stack)
+t_node	*go_to_last(t_node *stack)
 {
 	t_node	*last;
 
@@ -61,18 +58,23 @@ t_node	*got_to_last(t_node *stack)
 
 void	push_top(t_node **src, t_node **dst)
 {
-	t_node	*last_src;
-	t_node	*last_dst;
+	t_node	*temp;
 
 	if (!*src)
 		return ;
-	last_src = got_to_last(*src);
-	if (!*dst)
-		
-	last_dst = got_to_last(*dst);
-	last_src->prev->next = NULL;
-	last_dst->next = last_src;
-	last_src->prev = last_dst;
+	temp = *src;
+	if (*dst == NULL)
+	{
+		*dst = *src;
+		(*dst)->next = NULL;
+		(*dst)->prev = NULL;
+	}
+	else
+	{
+		(*src)->next = *dst;
+		(*dst)->prev = *src;
+	}
+	*src = temp->next;
 }
 
 void	pa(t_node *stack_b, t_node *stack_a)
@@ -89,5 +91,66 @@ void	pb(t_node *stack_a, t_node *stack_b)
 
 void	rotate(t_node **stack)
 {
-	
+	t_node	*last;
+
+	if ((!*stack) || ((*stack)->next == NULL))
+		return ;
+	last = go_to_last(*stack);
+	last->next = *stack;
+	*stack = (*stack)->next;
+	last->next->prev = last;
+	last->next->next = NULL;
+	(*stack)->prev = NULL;	
+}
+
+void	ra(t_node **stack_a)
+{
+	rotate(stack_a);
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_node **stack_b)
+{
+	rotate(stack_b);
+	write(1, "rb\n", 3);
+}
+
+void	rr(t_node **stack_a, t_node **stack_b)
+{
+	rotate(stack_a);
+	rotate(stack_b);
+	write(1, "rr\n", 3);
+}
+
+void	reverse_rotate(t_node **stack)
+{
+	t_node	*last;
+
+	if ((!*stack) || ((*stack)->next == NULL))
+		return ;
+	last = go_to_last(*stack);
+	last->next = *stack;
+	(*stack)->prev = last;
+	last->prev->next = NULL;
+	last->prev = NULL;
+	*stack = last;
+}
+
+void	rra(t_node **stack_a)
+{
+	reverse_rotate(stack_a);
+	write(1, "rra\n", 4);
+}
+
+void	rrb(t_node **stack_b)
+{
+	reverse_rotate(stack_b);
+	write(1, "rrb\n", 4);
+}
+
+void	rrr(t_node **stack_a, t_node **stack_b)
+{
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+	write(1, "rrr\n", 4);
 }
